6ce5cf831c6a42d848413ed7b47d070c49edb54d
luzhisheng <aiyingfeng110@qq.com>
2022-05-06 18:07:23 +0800

    猿人学第2题-js 混淆 - 动态cookie

---
+++ 猿人学第2题-js 混淆 - 动态cookie/案例.js
@@ -0,0 +1,18 @@
+_0x5500bb = {};
+
+function _0x443ca7(_0x48561e) {
+    _0x47cc1a = {},
+        _0x47cc1a[$dbsm_0x42c3(oQoQoq, qQQOOO) + '\x46\x59'] = _0x5500bb[$dbsm_0x42c3(LoLQoq, QiQqIq) + '\x5a\x64'],
+        _0x47cc1a['\x45\x67\x4e' + '\x71\x68'] = _0x5500bb[$dbsm_0x42c3(IQ00Qo, O0oqO0) + '\x73\x64'],
+        _0x37a87f = _0x47cc1a;
+    if (_0x5500bb[$dbsm_0x42c3(l0Oqoq, Qo01oq) + '\x76\x70'](_0x5500bb[$dbsm_0x42c3(qQll0q, QQloqo) + '\x4e\x6b'], _0x5500bb[$dbsm_0x42c3(qqLooq, oQ0liL) + '\x6f\x47'])) {
+        return _0x5500bb['\x64\x4a\x43' + '\x43\x58'](_0x57fdd5, _0x5500bb[$dbsm_0x42c3(lQl00o, Q10loO) + '\x43\x58'](_0x45dccd, _0x48561e));
+    } else {
+        return function(_0x2cf9d7) {}
+            [$dbsm_0x42c3(QOqqqQ, qQQIQq) + $dbsm_0x42c3(q1O0Qo, QOo1lq) + '\x75\x63\x74' + '\x6f\x72'](KKbXTR[$dbsm_0x42c3(Ql1lOo, QOOqqi) + '\x46\x59'])[$dbsm_0x42c3(QIqQlq, lqOqoO) + '\x6c\x79'](KKbXTR[$dbsm_0x42c3(OoOOIo, QqOOl0) + '\x71\x68']);
+    }
+}
+
+console.log(1111111);
+cookie = _0x443ca7(1651823474000);
+console.log(cookie);
