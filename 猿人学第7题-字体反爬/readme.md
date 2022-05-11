038673153bdeb4f279708817aadfee7977800b69
luzhisheng <aiyingfeng110@qq.com>
2022-05-11 13:53:36 +0800

    猿人学第7题-字体反爬

--- 猿人学第7题-字体反爬/readme.md
+++ 猿人学第7题-字体反爬/readme.md
@@ -1,3 +1,13 @@
-# 字体编辑器
+## 知识点
+
+字体编辑器
 
     http://font.qqe2.com/
+
+Python | fontTools的使用
+
+    https://zhuanlan.zhihu.com/p/350807659
+    
+Python爬虫---刷新你的认知，字体反爬并没有那么简单
+
+    https://zhuanlan.zhihu.com/p/99497149
--- 猿人学第7题-字体反爬/案例.py
+++ 猿人学第7题-字体反爬/案例.py
@@ -1,6 +1,5 @@
 from fontTools.ttLib import TTFont
 import requests
-import time
 import json
 import base64
 
@@ -25,25 +24,41 @@ class App(object):
         with open('font.woff', 'wb') as f:
             f.write(base64.decodebytes(b64_code.encode()))
 
-    def get_xml(self):
-        font = TTFont('5.woff')
-        font.saveXML('5.xml')
+    def get_xml(self, data):
+        font = TTFont('font.woff')
+        font.saveXML('movie.xml')
+        data_value = data.get('value').strip().replace('&#x', 'uni')
+        data_value_list = data_value.split(" ")
+        map_num_list = []
+        for data_v in data_value_list:
+            map_num = {
+                "10100100100101010010010010": '0',
+                "100110101001010101011110101000": '2',
+                "111111111111111": '4',
+                "1110101001001010110101010100101011111": '5',
+                "1001101111": '1',
+                "10010101001110101011010101010101000100100": '9',
+                "101010101101010001010101101010101010010010010101001000010": '8',
+                "10101100101000111100010101011010100101010100": '3',
+                "1111111": '7',
+                "10101010100001010111010101101010010101000": '6'
+            }
+            flags_num = list(font['glyf'][data_v].flags)
+            flags_num_str = "".join([str(flag) for flag in flags_num])
+            map_num_list.append(map_num[flags_num_str])
+        return "".join(map_num_list)
 
     def run(self):
-        data_sum = 0
         for page in range(1, 6):
-            # res = self.get_html("", page)
-            # woff = res.get('woff')
-            # self.with_ttf(woff)
-            self.get_xml()
-            exit()
-            # data_list = res.get('data')
-            # for data in data_list:
-            #     print(data.get('value'))
-            #     # data_sum += data.get('value')
-            # time.sleep(1)
+            res = self.get_html("", page)
+            woff = res.get('woff')
+            self.with_ttf(woff)
+            data_num = []
+            for data in res.get('data'):
+                map_num_str = self.get_xml(data)
+                data_num.append(map_num_str)
 
-        print(data_sum)
+            print(data_num)
 
 
 if __name__ == '__main__':
