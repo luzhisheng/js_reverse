eae820979edaf8ebc96cd8f4201af5c06ed19f20
luzhisheng <aiyingfeng110@qq.com>
2023-02-06 15:55:55 +0800

    59骚操作脏数据

---
+++ 猿人学练习/59骚操作脏数据/main.py
@@ -0,0 +1,32 @@
+import json
+import requests
+
+
+def challenge59(page):
+    url = "https://www.python-spider.com/api/challenge59"
+    payload = f"page={page}"
+    session = requests.session()
+    headers = {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }
+    session.headers = headers
+    response = session.request("POST", url, data=payload)
+    return response.text
+
+
+def run():
+    data_num = 0
+    for page in range(1, 101):
+        res_dict = json.loads(challenge59(page))
+        data_list = res_dict.get('data')
+        data_list_num = []
+        for data in data_list:
+            data_list_num.append(int(data.get('value')))
+            data_num += int(data.get('value'))
+        print(data_list_num, page)
+        print(data_num)
+    print(data_num)
+
+
+if __name__ == '__main__':
+    run()
