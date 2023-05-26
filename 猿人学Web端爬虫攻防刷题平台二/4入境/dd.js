3538ab55872939e6f26ddfce3b40576ce5fd481e
luzhisheng <aiyingfeng110@qq.com>
2023-05-26 13:45:50 +0800

    4入境

---
+++ 猿人学Web端爬虫攻防刷题平台二/4入境/dd.js
@@ -0,0 +1,481 @@
+function _$tU() {
+    var _$a = 'ghij';
+    var _$e = 'abcdef';
+    var _$r = 'k';
+    var _$c, _$v, _$s, _$n, _$f, _$t, _$i, _$o, _$k, _$b, _$l, _$h, _$g, _$u, _$p, _$y, _$w;
+    _$c = 8;
+    _$v = _$c;
+    while (!![]) {
+        switch (_$v) {
+            case 1:
+                _$l = _$l.j(_$BC);
+                _$v += 1;
+                break;
+            case 2:
+                if (_$h.c(5) > _$l.c(4)) {
+                    _$c = _$h;
+                    _$s = _$EC;
+                    _$n = _$c + _$s;
+                    _$h = _$n
+                }
+                _$v += 1;
+                break;
+            case 3:
+                _$k = 1;
+                _$v += 8;
+                break;
+            case 4:
+                _$k = _$b;
+                _$v += 15;
+                break;
+            case 5:
+                return _$u;
+            case 6:
+                for (_$f = _$h.length - 1; _$f >= 0; _$f--) {
+                    _$c = _$l;
+                    _$s = 'p';
+                    _$n = _$h;
+                    _$t = 'c';
+                    _$i = _$f;
+                    _$o = 'k';
+                    _$c[_$s](_$o)
+                }
+                _$v += 6;
+                break;
+            case 7:
+                _$h = _$h.j(_$BC);
+                _$v -= 3;
+                break;
+            case 8:
+                _$k = 'Zl';
+                _$b = 1;
+                _$l = [];
+                _$h = _$e + _$a + _$r;
+                _$v -= 2;
+                break;
+            case 9:
+                _$g = _$h;
+                _$v += 7;
+                break;
+            case 10:
+                _$l = _$g;
+                _$v -= 1;
+                break;
+            case 11:
+                _$g = _$l + _$h;
+                _$v += 6;
+                break;
+            case 12:
+                _$b = _$b + 1;
+                _$v -= 11;
+                break;
+            case 13:
+                _$h += _$ZC;
+                _$v += 10;
+                break;
+            case 14:
+                _$h += _$zC;
+                _$v += 10;
+                break;
+            case 15:
+                _$l += _$GC;
+                _$v -= 10;
+                break;
+            case 16:
+                _$b = _$b.p(_$k);
+                _$v += 6;
+                break;
+            case 17:
+                _$h = [];
+                _$v += 3;
+                break;
+            case 18:
+                for (_$f = _$h.length - 1; _$f >= 4; _$f--) {
+                    _$c = _$h;
+                    _$s = _$LC;
+                    _$n = _$g;
+                    _$t = _$_C;
+                    _$i = _$f;
+                    _$o = _$n[_$t](_$i);
+                    _$c[_$s](_$o)
+                }
+                _$v -= 11;
+                break;
+            case 19:
+                _$h += _$zC;
+                _$v -= 6;
+                break;
+            case 20:
+                _$u = _$BC;
+                _$v -= 2;
+                break;
+            case 21:
+                _$h += _$_C;
+                _$v -= 7;
+                break;
+            case 22:
+                if (_$h.c(5) > _$l.c(7)) {
+                    _$c = _$h;
+                    _$s = _$NC;
+                    _$n = _$c + _$s;
+                    _$h = _$n
+                }
+                _$v -= 7;
+                break;
+            case 23:
+                _$b = [210944, 206848, 237568, 172032, 215040, 223232, 206848];
+                _$v -= 2;
+                break;
+            case 24:
+                for (_$p = 0; _$p < _$b.length; _$p++) {
+                    _$c = _$u;
+                    _$s = _$b;
+                    _$n = _$p;
+                    _$t = _$s[_$n];
+                    _$i = 11;
+                    _$o = _$t >> _$i;
+                    _$y = _$bW(_$o);
+                    _$w = _$c + _$y;
+                    _$u = _$w
+                }
+                _$v -= 14;
+                break
+        }
+    }
+}
+
+
+function _$IU(_$a, _$e) {
+    var _$r = 'lmnop';
+    var _$c = 'abacd';
+    var _$v = 'GHJKLM';
+    var _$s = 'YZ012';
+    var _$n = '34567';
+    var _$f = 'qrstuv';
+    var _$t = 'NOPQRS';
+    var _$i = '89';
+    var _$o = 'efghjk';
+    var _$k = 'TUVWX';
+    var _$b = 'wxyz';
+    var _$l = 'ABCDEF';
+    var _$h, _$g, _$u, _$p, _$y, _$w, _$d, _$M, _$I, _$A, _$S, _$m, _$T, _$j, _$C, _$D, _$K, _$Q, _$W, _$Y, _$U,
+        _$O, _$P, _$G, _$N, _$Z, _$z, _$E, _$R;
+    _$h = _$c + _$o + _$r + _$f + _$b + _$l + _$v + _$t + _$k + _$s + _$n + _$i;
+    _$g = _$h;
+
+    function _$_(_$a, _$e, _$r) {
+        var _$c, _$v, _$s, _$n, _$f, _$t, _$i, _$o, _$k, _$b, _$l, _$h, _$u, _$p, _$y, _$w, _$d, _$M, _$I, _$A, _$S,
+            _$m, _$T;
+        _$c = Math;
+        _$v = 'floor';
+        _$s = Math;
+        _$n = 'random';
+        _$f = _$s[_$n]();
+        _$t = _$e;
+        _$i = _$a;
+        _$o = _$t - _$i;
+        _$k = 1;
+        _$b = _$o + _$k;
+        _$l = _$f * _$b;
+        _$h = _$a;
+        _$u = _$l + _$h;
+        _$p = _$c[_$v](_$u);
+        _$y = _$p;
+        _$w = _$g;
+        _$d = 'length';
+        _$M = _$w[_$d];
+        _$I = 10;
+        _$A = _$M - _$I;
+        _$S = _$A;
+        _$c = _$r;
+        _$v = 0;
+        _$s = _$c == _$v;
+        _$n = _$s;
+        if (_$n) {
+            _$m = _$y;
+            _$T = _$S;
+            _$n = _$m >= _$T
+        }
+        if (_$n) {
+            _$c = _$a;
+            _$v = _$e;
+            _$s = _$r;
+            _$n = _$_(_$c, _$v, _$s);
+            _$y = _$n
+        }
+        _$c = _$y;
+        return _$c
+    }
+
+    function _$L(_$a) {
+        var _$e, _$r, _$c, _$v, _$s, _$n, _$f, _$t, _$i, _$o, _$k, _$b;
+        _$e = 0;
+        _$r = _$e;
+        _$c = _$g;
+        _$v = 'length';
+        _$s = _$c[_$v];
+        _$n = 1;
+        _$f = _$s - _$n;
+        _$t = _$f;
+        _$i = '';
+        _$o = _$i;
+        _$e = _$a;
+        _$c = _$e;
+        if (!_$c) {
+            _$c = 15
+        }
+        _$a = _$c;
+        for (_$k = 0; _$k < _$a; _$k++) {
+            _$e = _$r;
+            _$c = _$t;
+            _$v = _$k;
+            _$s = _$_(_$e, _$c, _$v);
+            _$b = _$s;
+            _$e = _$g;
+            _$c = _$b;
+            _$v = _$e[_$c];
+            _$o += _$v
+        }
+        _$e = _$o;
+        return _$e
+    }
+
+    _$h = 44;
+    _$u = _$L(_$h);
+    _$a = _$u;
+    _$h = 0;
+    _$p = _$h;
+    _$u = 22;
+    _$y = 23;
+    _$w = 17;
+    _$d = 10;
+    _$M = 7;
+    _$I = 9;
+    _$A = 25;
+    _$S = 5;
+    _$m = 4;
+    _$T = 3;
+    _$j = 2;
+    _$C = 12;
+    _$D = 18;
+    _$K = 13;
+    _$Q = [_$u, _$y, _$w, _$d, _$M, _$I, _$A, _$S, _$m, _$T, _$j, _$C, _$D, _$K];
+    _$W = _$Q;
+    while (!![]) {
+        switch (_$W[_$p++]) {
+            case 1:
+                return _$SU(1253 - 934, _$YW).slice(8, 125);
+            case 2:
+                if (_$z < 0) {
+                    _$h = _$Z;
+                    _$u = _$N;
+                    _$y = _$E;
+                    _$w = _$u / _$y;
+                    _$d = _$h >> _$w;
+                    _$M = _$N;
+                    _$I = _$d >> _$M;
+                    _$z = _$I
+                }
+                break;
+            case 3:
+                if (_$N + _$E < _$Z) {
+                    _$h = _$N;
+                    _$u = _$z;
+                    _$y = _$E;
+                    _$w = _$u + _$y;
+                    _$d = _$h >> _$w;
+                    _$M = _$N;
+                    _$I = _$Z;
+                    _$A = _$M - _$I;
+                    _$S = _$d >> _$A;
+                    _$m = _$E;
+                    _$T = _$S >> _$m;
+                    _$E = _$T
+                }
+                break;
+            case 4:
+                _$sU();
+                break;
+            case 5:
+                if (_$Z + _$z > 0) {
+                    _$h = _$Z;
+                    _$u = _$E;
+                    _$y = _$h + _$u;
+                    _$E = _$y;
+                    _$h = _$Z;
+                    _$u = _$E;
+                    _$y = _$h - _$u;
+                    _$z = _$y
+                }
+                break;
+            case 6:
+                if (!_$N) {
+                    _$h = 5;
+                    _$u = _$z;
+                    _$y = _$h + _$u;
+                    _$w = 3;
+                    _$d = _$y >> _$w;
+                    _$N = _$d
+                }
+                break;
+            case 7:
+                _$Z = -5;
+                _$Y = [20, 11, 6, 15, 8, 24, 19, 21, 14, 1];
+                break;
+            case 8:
+                _$cU(this[_$fU()]);
+                break;
+            case 9:
+                _$P = _$a;
+                break;
+            case 10:
+                if (_$N && !_$Z) {
+                    _$h = _$z;
+                    _$u = 3;
+                    _$y = _$h % _$u;
+                    _$E = _$y;
+                    _$h = _$Z;
+                    _$u = _$E;
+                    _$y = _$h + _$u;
+                    _$E = _$y
+                }
+                break;
+            case 11:
+                if (!_$Z) {
+                    _$h = _$z;
+                    _$u = 2;
+                    _$y = _$Z;
+                    _$w = _$u + _$y;
+                    _$d = _$N;
+                    _$M = _$w - _$d;
+                    _$I = _$h << _$M;
+                    _$z = _$I
+                }
+                break;
+            case 12:
+                _$yW[_$bU()][_$lU()][_$wU()] = _$G;
+                break;
+            case 13:
+                if (_$Z + _$z > 0) {
+                    _$h = _$z;
+                    _$u = 2;
+                    _$y = _$h << _$u;
+                    _$z = _$y;
+                    _$h = _$z;
+                    _$u = _$E;
+                    _$y = _$E;
+                    _$w = _$u + _$y;
+                    _$d = _$h >> _$w;
+                    _$M = _$N;
+                    _$I = _$d >> _$M;
+                    _$Z = _$I;
+                    _$h = _$Z;
+                    _$u = _$E;
+                    _$y = _$h / _$u;
+                    _$E = _$y
+                }
+                break;
+            case 14:
+                _$yW[_$SU(724 - 648)][_$SU(904 - 223)][_$ZC] = (new Date)[_$SU(509 - 350)]() - _$G;
+                break;
+            case 15:
+                _$aU(_$P, _$e);
+            case 16:
+                if (_$Z + _$E > 0) {
+                    _$h = _$z;
+                    _$u = 4;
+                    _$y = _$Z;
+                    _$w = _$u + _$y;
+                    _$d = _$h >> _$w;
+                    _$M = 3;
+                    _$I = _$Z;
+                    _$A = _$M * _$I;
+                    _$S = _$z;
+                    _$m = _$A + _$S;
+                    _$T = _$d >> _$m;
+                    _$j = 2;
+                    _$C = _$T << _$j;
+                    _$E = _$C
+                }
+                break;
+            case 17:
+                break;
+            case 18:
+                if (_$z + _$E < 0) {
+                    _$h = _$N;
+                    _$u = _$z;
+                    _$y = _$E;
+                    _$w = _$u * _$y;
+                    _$d = _$h << _$w;
+                    _$M = _$N;
+                    _$I = _$d >> _$M;
+                    _$Z = _$I
+                }
+                _$R.apply(_$W, _$Y);
+                break;
+            case 19:
+                _$O = _$nU(_$U, _$P);
+                break;
+            case 20:
+                _$U = _$dU(_$P, _$e, _$G);
+                break;
+            case 21:
+                _$SU(232 - 62, _$O, _$P, this[_$SU(872 - 130)], _$G);
+                break;
+            case 22:
+                _$G = (new Date)[_$tU()]();
+                _$N = 1;
+                _$Z = -1;
+                _$z = 2;
+                _$E = 0;
+                _$R = _$W.p;
+                break;
+            case 23:
+                if (_$N + _$Z > 0) {
+                    _$h = _$z;
+                    _$u = 3;
+                    _$y = _$h >> _$u;
+                    _$E = _$y;
+                    _$h = _$Z;
+                    _$u = _$E;
+                    _$y = _$h + _$u;
+                    _$E = _$y;
+                    _$h = _$N;
+                    _$u = _$z;
+                    _$y = _$E;
+                    _$w = _$u * _$y;
+                    _$d = _$h >> _$w;
+                    _$M = _$N;
+                    _$I = _$d >> _$M;
+                    _$Z = _$I;
+                    _$h = _$Z;
+                    _$u = _$E;
+                    _$y = _$h / _$u;
+                    _$E = _$y
+                }
+                break;
+            case 24:
+                _$iU();
+                break;
+            case 25:
+                if (_$N + _$Z + _$N > 0) {
+                    _$h = _$N;
+                    _$u = _$z;
+                    _$y = _$E;
+                    _$w = _$u + _$y;
+                    _$d = _$h >> _$w;
+                    _$M = _$N;
+                    _$I = _$d >> _$M;
+                    _$Z = _$I;
+                    _$h = _$Z;
+                    _$u = _$E;
+                    _$y = _$h + _$u;
+                    _$E = _$y
+                }
+                break
+        }
+    }
+}
+
+
+_$IU("YtNMIT/vOuJkScU6ZqG+zY3tfyyrHuzHO4glJQaz/OU=", 1684730146936)
