33341fab48d0b5dba37de1271ec96a9efb8909f6
luzhisheng <aiyingfeng110@qq.com>
2023-06-18 23:47:47 +0800

    猿人学第14题-备而后动-勿使有变

---
+++ 猿人学Web端爬虫攻防刷题平台/猿人学第14题-备而后动-勿使有变/14.js
@@ -0,0 +1,37 @@
+var $_0xad4684 = function () {
+    var _0x17da04 = !![];
+
+    return function (_0xb2ea18, _0x1bc01e) {
+        if (_0x17da04) {
+            var _0x3e7529 = function () {
+                if (_0x1bc01e) {
+                    var _0x474987 = _0x1bc01e.apply(_0xb2ea18, arguments);
+
+                    _0x1bc01e = null;
+                    return _0x474987;
+                }
+            };
+        } else {
+            var _0x3e7529 = function () {
+            };
+        }
+
+        _0x17da04 = ![];
+        return _0x3e7529;
+    };
+}();
+
+
+
+var $_0x158989 = $_0xad4684(this, function () {
+    var _0x92502a = function () {
+        var _0x3733a3 = _0x92502a.constructor("return /\" + this + \"/")().compile("^([^ ]+( +[^ ]+)+)+[^ ]}");
+
+        return !_0x3733a3.test($_0x158989);
+    };
+
+    return _0x92502a();
+});
+$_0x158989();
+window.v14 = "e9h6rom5gz8cpjn";
+window.v142 = "36703668750";
