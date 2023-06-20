bc47f292716741712178c1cb2a1964de7343637b
luzhisheng <aiyingfeng110@qq.com>
2023-06-21 00:36:00 +0800

    猿人学第8题-验证码图文点选

--- 猿人学Web端爬虫攻防刷题平台/猿人学第8题-验证码图文点选/gan_rao_xian.py
+++ 猿人学Web端爬虫攻防刷题平台/猿人学第8题-验证码图文点选/gan_rao_xian.py
@@ -15,9 +15,15 @@ def turn_white(img, r1, g1, b1):
 def processing_image(img_file, standard=205):
     """ 1.将图片进行降噪处理, 通过二值化去掉后面的背景色并加深文字对比度 """
     img = Image.open(img_file)
+    # for i in range(img.size[0]):
+    #     for j in range(img.size[1]):
+    #         r, g, b = img.getpixel((i, j))
+    #         print([r, g, b])
+
 
     # np.unique()该函数是去除数组中的重复数字，并进行排序之后输出，这里返回的是所有像素rgb值，以及对应的数量
-    # colors, counts = np.unique(np.array(im).reshape(-1, 3), axis=0, return_counts=True)
+    colors, counts = np.unique(np.array(img).reshape(-1, 3), axis=0, return_counts=True)
+    print(colors, counts)
     # ct = np.sort(counts)
     # top2_counts = ct[-2:].tolist()
     # print(np.array(im).reshape(-1, 3))
