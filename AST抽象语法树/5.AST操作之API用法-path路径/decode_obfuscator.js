414311bd0e4989ca31154d0fc1b45afafe5848da
aiyingfeng <aiyingfeng110@qq.com>
2023-07-17 18:36:17 +0800

    AST操作之API用法-path路径

---
+++ AST抽象语法树/5.AST操作之API用法-path路径/decode_obfuscator.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const {parse} = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const generator = require("@babel/generator").default;
+let encode_file = "./encode.js";
+
+let js_code = fs.readFileSync(encode_file, {encoding: "utf-8"});
+let ast = parse(js_code);
+
+const visitor = {
+    enter(path)
+    {
+        if(path.isNumericLiteral())
+        {
+            path.remove()
+        }
+    },
+}
+
+traverse(ast, visitor);
+
+// 写入文件
+let {code} = generator(ast);
+console.log(code)
+fs.writeFile('decode.js', code, (err) => {});
---
+++ AST抽象语法树/README.md
@@ -0,0 +1,5 @@
+# 参考文档
+
+深入学习 JavaScript 转译器 Babel ，AST还原混淆代码
+https://blog.csdn.net/weixin_52057903/article/details/129131582
+
