d5833b7d34d1e2d96b09ad6ce26823f4cc38c503
aiyingfeng <aiyingfeng110@qq.com>
2023-08-02 20:46:50 +0800

    pc_抖音破解

--- 抖音js逆向学习/pc_抖音破解/spider/douyin_search.py
+++ 抖音js逆向学习/pc_抖音破解/spider/douyin_search.py
@@ -27,6 +27,7 @@ def get_ac_nonce(encoded_text):
     }
     conn.request("GET", url=url, headers=headers)
     response = conn.getresponse()
+    print(response.read().decode())
     cookie_dict = get_cookie_dict(response)
     __ac_nonce = cookie_dict.get('__ac_nonce')
     conn.close()
@@ -41,7 +42,7 @@ def get_ttwid(ac_nonce, encoded_text):
                   '*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
         'cache-control': 'no-cache',
-        'cookie': f'__ac_nonce={ac_nonce}; __ac_signature=_02B4Z6wo00f01qT38OwAAIDBlV1UaegcPdak1.RAAM4EChrzmTNVTdns5RT2dW5igX46-5oWSdY3gufX2aITjm7dpjjH8v3edtU9Z8z1uZ5Qq6pR7L-ZcR3HDMDQa0lX9Q7Op7IQTaTqlAnY70;',
+        'cookie': f'__ac_nonce={ac_nonce}; __ac_signature=_02B4Z6wo00f01Uzv-9gAAIDCfUVfXfA0ld1Mz.9AADf1d8;',
         'pragma': 'no-cache',
         'referer': f'https://www.douyin.com/search/{encoded_text}?source=normal_search&type=user',
         'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
@@ -54,6 +55,7 @@ def get_ttwid(ac_nonce, encoded_text):
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/114.0.0.0 Safari/537.36'
     }
+    print(headers['cookie'])
     conn.request("GET", url=url, headers=headers)
     response = conn.getresponse()
     # print(response.read().decode())
@@ -62,19 +64,22 @@ def get_ttwid(ac_nonce, encoded_text):
         ttwid = cookie_dict.get('ttwid')
         conn.close()
         return ttwid
-    except AttributeError as e:
+    except Exception as e:
         print(f"报错{e}")
 
 
 def get_cookie_dict(response):
     cookies_dict = {}
-    cookie_str = response.getheader("Set-Cookie").replace('secure, ', '')
-    cookies_list = cookie_str.split(";")
-    for cookie in cookies_list:
-        if "=" in cookie.strip():
-            name, value = cookie.strip().split("=", 1)
-            cookies_dict[name] = value
-    return cookies_dict
+    if response.getheader("Set-Cookie"):
+        cookie_str = response.getheader("Set-Cookie").replace('secure, ', '')
+        cookies_list = cookie_str.split(";")
+        for cookie in cookies_list:
+            if "=" in cookie.strip():
+                name, value = cookie.strip().split("=", 1)
+                cookies_dict[name] = value
+        return cookies_dict
+    else:
+        raise ValueError('Set-Cookie为空')
 
 
 if __name__ == '__main__':
---
+++ 抖音js逆向学习/pc_抖音破解/tool/run.py
@@ -0,0 +1,41 @@
+import requests
+import execjs
+
+
+def get_ac_sign(ac_nonce):
+    with open('../node/_ac_signature.js', 'r', encoding='utf-8') as f:
+        b = f.read()
+    c = execjs.compile(b)
+    d = c.call('get_ac_signature', ac_nonce)
+    return d
+
+
+def run():
+    sess = requests.session()
+    url = "https://www.douyin.com/user/MS4wLjABAAAA8Ag2TMrWvfZWpLywypFRGhoL2_A8FdXawWfuh29bJWCJ_BCr23KY0S9O76eC4_oC"
+    __ac_nonce = "06384169e00527ffaf338"
+    __ac_signature = get_ac_sign(__ac_nonce)
+    sess.headers = {
+        'authority': 'www.douyin.com',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'zh-CN,zh;q=0.9,en-XA;q=0.8,en;q=0.7,zh-TW;q=0.6',
+        'cache-control': 'max-age=0',
+        'dnt': '1',
+        'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'cookie': f'__ac_nonce={__ac_nonce}; __ac_signature={__ac_signature}',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+    }
+    payload = {}
+    response = sess.get(url, data=payload)
+    print(response.text)
+
+
+if __name__ == '__main__':
+    run()
