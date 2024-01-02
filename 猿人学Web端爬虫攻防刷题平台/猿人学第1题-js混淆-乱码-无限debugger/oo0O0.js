a3f0c71b966d966dcec2716f20345cb4dc85ea99
luzhisheng <aiyingfeng110@qq.com>
2024-01-03 01:04:06 +0800

    增加测试代码

---
+++ 猿人学Web端爬虫攻防刷题平台/猿人学第1题-js混淆-乱码-无限debugger/oo0O0.js
@@ -0,0 +1,48 @@
+function oo0O0(mw) {
+    window.b = '';
+    for (var i = 0, len = window.a.length; i < len; i++) {
+        console.log(window.a[i]);
+        window.b += String[document.e + document.g](window.a[i][document.f + document.h]() - i - window.c)
+    }
+    var U = ['W5r5W6VdIHZcT8kU', 'WQ8CWRaxWQirAW=='];
+    var J = function (o, E) {
+        o = o - 0x0;
+        var N = U[o];
+        if (J['bSSGte'] === undefined) {
+            var Y = function (w) {
+                var m = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=',
+                    T = String(w)['replace'](/=+$/, '');
+                var A = '';
+                for (var C = 0x0, b, W, l = 0x0; W = T['charAt'](l++); ~W && (b = C % 0x4 ? b * 0x40 + W : W, C++ % 0x4) ? A += String['fromCharCode'](0xff & b >> (-0x2 * C & 0x6)) : 0x0) {
+                    W = m['indexOf'](W)
+                }
+                return A
+            };
+            var t = function (w, m) {
+                var T = [], A = 0x0, C, b = '', W = '';
+                w = Y(w);
+                for (var R = 0x0, v = w['length']; R < v; R++) {
+                    W += '%' + ('00' + w['charCodeAt'](R)['toString'](0x10))['slice'](-0x2)
+                }
+                w = decodeURIComponent(W);
+                var l;
+                for (l = 0x0; l < 0x100; l++) {
+                    T[l] = l
+                }
+                for (l = 0x0; l < 0x100; l++) {
+                    A = (A + T[l] + m['charCodeAt'](l % m['length'])) % 0x100, C = T[l], T[l] = T[A], T[A] = C
+                }
+                l = 0x0, A = 0x0;
+                for (var L = 0x0; L < w['length']; L++) {
+                    l = (l + 0x1) % 0x100, A = (A + T[l]) % 0x100, C = T[l], T[l] = T[A], T[A] = C, b += String['fromCharCode'](w['charCodeAt'](L) ^ T[(T[l] + T[A]) % 0x100])
+                }
+                return b
+            };
+            J['luAabU'] = t, J['qlVPZg'] = {}, J['bSSGte'] = !![]
+        }
+        var H = J['qlVPZg'][o];
+        return H === undefined ? (J['TUDBIJ'] === undefined && (J['TUDBIJ'] = !![]), N = J['luAabU'](N, E), J['qlVPZg'][o] = N) : N = H, N
+    };
+    eval(atob(window['b'])[J('0x0', ']dQW')](J('0x1', 'GTu!'), '\x27' + mw + '\x27'));
+    return ''
+};
