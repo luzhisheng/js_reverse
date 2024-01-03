6592197bdbdf163e104a0232ec544acc23e4c71f
luzhisheng <aiyingfeng110@qq.com>
2024-01-04 02:09:11 +0800

    增加测试代码

---
+++ 猿人学Web端爬虫攻防刷题平台/猿人学第2题-js-混淆-动态cookie-hook-cookie/检测代码.js
@@ -0,0 +1,38 @@
+setInterval(function () {
+    $dbsm_0x13b17a();
+}, 4000);
+
+function $dbsm_0x13b17a(_0x705a6b) {
+    console.log(_0x705a6b);
+    function _0x51c054(_0xcee267) {
+        // 判断是否是字符串，如果不是那就执行死循环内存爆掉
+        if (typeof _0xcee267 === "string") {
+            return function (_0x2abac3) {
+            }.constructor("while (true) {}").apply("counter");
+        } else {
+            if (("" + _0xcee267 / _0xcee267).length !== 1 || _0xcee267 % 20 === 0) {
+                (function () {
+                    return !![];
+                }).constructor("debugger").call("action");
+            } else {
+                (function () {
+                    return ![];
+                }).constructor("debugger").apply("stateObject");
+            }
+        }
+
+        _0x51c054(++_0xcee267);
+    }
+
+    try {
+        if (_0x705a6b) {
+            // 返回函数本身
+            return _0x51c054;
+        } else {
+            // 执行函数直接卡死
+            _0x51c054(0);
+        }
+    } catch (_0x10d27f) {
+        console.log(_0x10d27f);
+    }
+}
