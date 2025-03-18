f192f6695b1023509405859ebda6f86cb49bbde3
luzhisheng <aiyingfeng110@qq.com>
2025-03-18 23:39:54 +0800

    猿人学第18题-jsvmp 洞察先机

---
+++ cpp/HelloWorld.cpp
@@ -0,0 +1,26 @@
+//
+// Created by aiyin on 2024/3/21.
+//
+
+// 头文件 导入 iostream 库
+#include<iostream>
+
+// 主函数
+//int main(){
+//    // std 命名空间，指明 cout 所指的命名空间
+//    std::cout << "Hello World" << std::endl;
+//}
+
+// 添加了 using namespace std; 就可以省略 std 命名空间
+using namespace std;
+//int main(){
+//    // std 命名空间，指明 cout输出 所指的命名空间
+//    cout << "Hello World" << endl;
+//}
+
+// cin 等待键盘输入
+int main(){
+    // std 命名空间，指明 cout输出 所指的命名空间
+    cout << "Hello World" << endl;
+    cin.get();
+}
---
+++ cpp/HelloWorld.h
@@ -0,0 +1,16 @@
+//
+// Created by aiyin on 2024/3/21.
+//
+
+#ifndef CPP_HELLOWORLD_H
+#define CPP_HELLOWORLD_H
+
+namespace HelloWorld {
+
+    class HelloWorld {
+
+    };
+
+} // HelloWorld
+
+#endif //CPP_HELLOWORLD_H
